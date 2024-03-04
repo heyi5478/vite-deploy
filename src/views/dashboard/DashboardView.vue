93c49490e2d0bbf1c8160488bb4084d8e396b4e7
YI-JYUN HE <jobs.steve54@gmail.com>
2024-03-04 13:18:20 +0800

    git add

---
+++ src/views/dashboard/DashboardView.vue
@@ -0,0 +1,38 @@
+<template>
+    <h2>這是後台</h2>
+    <nav>
+    <RouterLink to="/admin/products">產品列表</RouterLink> |
+    <RouterLink to="/admin/order">訂單列表</RouterLink> |
+    <RouterLink to="/">回到前台</RouterLink>
+    </nav>
+    <RouterView></RouterView>
+</template>
+
+<script>
+import axios from 'axios';
+
+const { VITE_URL } = import.meta.env;
+
+export default {
+  methods: {
+    checkAdmin() {
+      const url = `${VITE_URL}api/user/check`;
+      axios.post(url)
+        .then((res) => {
+          console.log('驗證成功：', res.data.success);
+        })
+        .catch(() => {
+        //   window.location = 'login.html';
+          this.$router.push('/login');
+        });
+    },
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin();
+  },
+};
+</script>
